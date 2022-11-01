dfe01db53bb48c7e4fb83f45af2faff2cc442128
rodrigo <malr.105xx@gmail.com>
2022-11-01 13:41:44 -0600

    first-commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# NodeJS TS Configuración basica
---
+++ src/app.ts
@@ -0,0 +1,27 @@
+import express, { Application } from "express";
+import { Request, Response } from "express"; //Se va a quitar
+import morgan from "morgan";
+import path from "path"
+import dotenv from "dotenv";
+dotenv.config();
+
+const app: Application = express();
+
+
+//settings
+app.set("port", process.env.PORT || 4000);
+app.set("view engine","ejs");
+app.set('views', path.join(__dirname, './views'));
+
+
+
+//middlewares
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,'./public')))
+
+
+app.use("/",(req:Request,res:Response) =>{res.render("registro-citas-completo");});
+
+export default app;
