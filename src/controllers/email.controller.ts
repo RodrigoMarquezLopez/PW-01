9a1775f0928f2756ca4467ae560386eb38e97180
Christian-PR <chris20032001@hotmail.com>
2022-12-05 19:25:59 -0600

    correo con fecha

--- src/controllers/email.controller.ts
+++ src/controllers/email.controller.ts
@@ -21,6 +21,7 @@ export async function sendMail(req: Request, res: Response) {
       const cita1 = await Cita.findByPk(idCita);
       const idCita1 = cita1?.getDataValue('idCita');
       const hora = cita1?.getDataValue('hora') + " hrs.";
+      const fecha = " "+ cita1?.getDataValue('fecha');
 
       const idDoctor = cita1?.getDataValue('idDoctor');
       const Doc = await Doctor.findByPk(idDoctor);
@@ -29,7 +30,7 @@ export async function sendMail(req: Request, res: Response) {
       const nombreDoctor = "Dr. "+ personaDoctor?.getDataValue('nombres') + " "+personaDoctor?.getDataValue('apellidoP') +" "+ personaDoctor?.getDataValue('apellidoM');
 
 
-      const data ={idCita1,hora,nombreDoctor};
+      const data ={idCita1,fecha,hora,nombreDoctor};
 
       const htmlContent = await ejsLibrary.renderFileHtml({ file: "cuerpo-correo.ejs", data});
 
@@ -54,6 +55,50 @@ export async function sendMail(req: Request, res: Response) {
     
       
   }
+
+  export async function sendMailAviso(idCita:number | undefined ,idPersona:number | undefined) {
+    
+    
+    const persona = await Persona.findByPk(idPersona);
+    const correo = persona?.getDataValue('correo');
+
+    const cita1 = await Cita.findByPk(idCita);
+    const idCita1 = cita1?.getDataValue('idCita');
+    const hora = cita1?.getDataValue('hora') + " hrs.";
+
+    const idDoctor = cita1?.getDataValue('idDoctor');
+    const Doc = await Doctor.findByPk(idDoctor);
+    const idpersonaDoctor = Doc?.getDataValue('idPersona');
+    const personaDoctor = await Persona.findByPk(idpersonaDoctor);
+    const nombreDoctor = "Dr. "+ personaDoctor?.getDataValue('nombres') + " "+personaDoctor?.getDataValue('apellidoP') +" "+ personaDoctor?.getDataValue('apellidoM');
+
+
+    const data ={idCita1,hora,nombreDoctor};
+
+    const htmlContent = await ejsLibrary.renderFileHtml({ file: "cuerpo-correo.ejs", data});
+
+    try {
+      let info = await emailer.sendMail({
+          from: '"Médicos Especialistas 🏥" <medicosespecialistas.clinica@gmail.com>', // sender address
+          to:     correo, // list of receivers 
+          subject: "Confirma tu cita" , // Subject line
+          text: " ", // plain text body
+          html: htmlContent, // html body
+        });
+    } catch (error) {
+      console.log("nel prro");
+    }
+
+    await Cita.update({ estado:"confirmada" }, {
+      where: {
+        idCita
+      }
+    });
+    
+  
+    
+}
+
 
  
 
